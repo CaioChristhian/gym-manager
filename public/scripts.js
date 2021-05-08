deeffc1e8a2f05f90d4cf46d9b3d9e0cc34c35cb
Caio Christhian <caiochristhianz@gmail.com>
2021-05-07 21:56:24 -0300

    first commit

---
+++ public/scripts.js
@@ -0,0 +1,16 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+    card.addEventListener('click', function(){
+        const videoId = card.getAttribute('id');
+        window.location.href = `/video?id=${videoId}`
+    })
+}
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ''
+})
+
+
---
+++ routes.js
@@ -0,0 +1,37 @@
+// HTTP VERBS
+// GET : Receber RESOURCE
+// POST : Criar um Novo RESOURCE com dados enviados
+// PUT : Atualizar RESOURCE
+// DELETE : Deletar RESOURCE
+
+const express = require('express')
+const routes = express.Router()
+const instructors = require('./instructors')
+
+routes.get('/', function(req, res) {
+    return res.redirect("/instructors")
+})
+
+routes.get('/instructors', function(req, res) {
+    return res.render("instructors/index")
+})
+
+routes.get('/instructors/create', function(req,res){
+    return res.render('instructors/create')
+})
+
+routes.get('/instructors/:id', instructors.show)
+
+routes.get('/instructors/:id/edit', instructors.edit)
+
+routes.post("/instructors", instructors.post)
+
+routes.put("/instructors", instructors.put)
+
+routes.delete("/instructors", instructors.delete)
+
+routes.get('/members', function(req, res) {
+    return res.render("members")
+})
+
+module.exports = routes
