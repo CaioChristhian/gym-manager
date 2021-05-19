dbe06d7ef91cd5a52f87fb36928a2dac10ca27f7
Caio Christhian <caiochristhianz@gmail.com>
2021-05-19 17:17:07 -0300

    final

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-    age: function age(timestamp) {
+    age(timestamp) {
         const today = new Date()
         const birthDate = new Date(timestamp)
     
@@ -13,7 +13,7 @@ module.exports = {
         return age
     },
 
-    date: function(timestamp) {
+    date(timestamp) {
         const date = new Date(timestamp)
 
         const year = date.getUTCFullYear()
@@ -25,7 +25,8 @@ module.exports = {
             month,
             year,
             iso: `${year}-${month}-${day}`,
-            birthDay: `${day}/${month}`
+            birthDay: `${day}/${month}`,
+            format: `${day}/${month}/${year}`
         }
     }
 }
